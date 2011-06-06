9414ad2cae7dc9357ded7393257d2d2cab214b8c
Joel Nystrom <joeny878@student.liu.se>
2011-06-06 23:25:21 +0200

    Changed About Us

--- CodeIgniter/application/views/tabs/about us_view.php
+++ CodeIgniter/application/views/tabs/about us_view.php
@@ -1,6 +1,11 @@
-<div class="container10">
-<?php echo 'About Us' ?>
+<div class="container50">
+<?php //echo 'About Us' ?>
 <br />
-<?php echo $this->calendar->generate();?>
+
+<p class="developers"> This page is created by <br /> <a href="mailto:joeny878@student.liu.se?Subject=TDDD27%20Homepage">
+Joel Nystr&#246m </a> <br /> and <br /><a href="mailto:mikkl381@student.liu.se?Subject=TDDD27%20Homepage">
+Mikael Klahr</a></p>
+
+<?php //echo $this->calendar->generate();?>
 <br />
 </div>
